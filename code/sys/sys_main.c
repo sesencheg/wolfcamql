833f985115e6abd9f291ae01d902ea2464c7b87c
sesencheg <xekse@mail.ru>
2025-05-27 20:32:45 +0500

    Update sys_main.c

--- code/sys/sys_main.c
+++ code/sys/sys_main.c
@@ -347,12 +347,10 @@ Sys_Init
 */
 void Sys_Init(void)
 {
-	Cmd_AddCommand("backtrace", Sys_Backtrace_f);
 	Cmd_AddCommand( "in_restart", Sys_In_Restart_f );
 	Cvar_Set( "arch", OS_STRING " " ARCH_STRING );
 	Cvar_Set( "username", Sys_GetCurrentUser( ) );
 }
-
 
 
 /*
