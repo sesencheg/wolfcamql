e48b3911ff3d0dd6e420db102b4b596a4010ac9c
sesen <xekse@mail.ru>
2025-06-02 11:20:01 +0500

    Update vm_interpreted.c

--- code/qcommon/vm_interpreted.c
+++ code/qcommon/vm_interpreted.c
@@ -590,8 +590,8 @@ nextInstruction2:
 			if ( programCounter == -1 ) {
 				goto done;
 			} else if ( (unsigned)programCounter >= vm->codeLength ) {
-				//Com_Error( ERR_DROP, "VM program counter out of range in OP_LEAVE" );
-				//return 0;
+				Com_Error( ERR_DROP, "VM program counter out of range in OP_LEAVE" );
+				return 0;
 			}
 			goto nextInstruction;
 
