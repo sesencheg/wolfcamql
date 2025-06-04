9917dde962cffbfb5755e27478cfb6a3819426db
sesen <xekse@mail.ru>
2025-06-04 13:07:03 +0500

    Update tr_mme.h

--- code/renderercommon/tr_mme.h
+++ code/renderercommon/tr_mme.h
@@ -24,7 +24,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define tr_mme_h_included
 
 #include "tr_common.h"
-#include <xmmintrin.h>
 
 #define BLURMAX 256
 //#define PASSMAX 256
